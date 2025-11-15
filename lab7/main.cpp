25155ae2120f9d4bfd6be8fe2599033d2ba72dd7
RoboMax143 <navas23154@gmail.com>
2025-11-15 11:32:28 +0200

    lab7

---
+++ lab7/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main() {
+	/*
+	double a = 3.14159;
+	double* pa = &a;
+
+	double* pd = pa+2;
+
+	std::cout << &pa << std::endl;
+	std::cout << &pd << std::endl;
+
+	std::cout << &pd - &pa << std::endl;
+
+	std::cout << *pa << std::endl;
+	std::cout << *pd << std::endl;
+	*/
+
+
+	char* pl = new char[8];
+
+	std::cout << *pl << std::endl;
+	std::cout << pl << std::endl;
+	std::cout << &pl << std::endl;
+
+	*pl = 'A';
+	*(pl + 1) = 'B';
+	*(pl + 2) = 'C';
+	*(pl + 3) = 'D';
+	*(pl + 4) = 'E';
+	*(pl + 5) = 'F';
+	*(pl + 6) = 'G';
+	*(pl + 7) = 'H';
+	
+	std::cout << *pl << std::endl;
+	std::cout << pl << std::endl;
+	std::cout << &pl << std::endl;
+}
